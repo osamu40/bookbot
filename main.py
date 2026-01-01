83c9960436b7ff69a8c7a955ed97ccf37f2fb355
osamu40 <r.rodrigues.338@gmail.com>
2026-01-01 14:44:38 -0300

    E: main.py now analyzes books on  provided path //finished

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import sys
+
 from stats import word_count, character_count, sorted_list
+
+
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -6,18 +10,24 @@ def get_book_text(file_path):
     return file_contents
 
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+        
+    book_text = get_book_text(sys.argv[1])
     count = word_count(book_text)
 
     print ("============ BOOKBOT ============")
-    print (f"Analyzing book found at ./books/frankenstein.txt")
+    print (f"Analyzing book found at {sys.argv[1]}")
     print ("----------- Word Count -----------")
     print (f"Found {count} total words")
     print ("--------- Character Count -------")
     
     ch_sorted = sorted_list(character_count(book_text))
     for char_dict in ch_sorted:
-        print (f"{char_dict["char"]}: {char_dict["count"]}")
+        print (f'{char_dict["char"]}: {char_dict["count"]}')
     
     print("============= END ===============")
     
